332e7ecaa884b2a3d8d4cea34a61570557e1660b
mdmishrat13 <mdmishrat13@gmail.com>
2022-09-15 19:53:20 +0600

    port updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(bodyParser.json())
